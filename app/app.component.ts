88b41cd0c4ace19583984540d2d0dea2db84a9d9
Niv <niv686@gmail.com>
2016-07-02 19:34:19 +0300

    added input databiding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,23 @@
 
 import { Component } from '@angular/core';
+import { Customer } from './customer.component';
 
 @Component({
     selector: 'my-app',
     template: `<h1>My First Angular 2 App</h1>
 
-                <p>ervervevrerv</p>
-                
-                <input type="text" name="lname">
+                and input:              
+                <input [(ngModel)]="customer.name" type="text" name="lname">
                 
                 <p>The sum of 1 + 1 is {{1 + 1}}</p>
-                <p>name of customer is: {{customer}}</p>
+                <p>name of customer is: {{customer.name}}</p>
              `
 
 })
-export class AppComponent { }
+export class AppComponent {
+customer: Customer = {
+    id: 1,
+    name: 'aaa'
+};
+
+}
