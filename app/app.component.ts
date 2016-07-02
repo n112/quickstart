da52e3f7b43bc5b5d891f365699a6855b69cbdf6
Niv <niv686@gmail.com>
2016-07-02 19:11:27 +0300

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
+
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>
+
+                <p>ervervevrerv</p>
+                
+                <input type="text" name="lname">
+                
+                <p>The sum of 1 + 1 is {{1 + 1}}</p>
+             `
+
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
